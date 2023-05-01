91f6a55ea16e880edd750404f864262a41520f83
BelicusBr <X70r@outlook.com>
2023-05-01 19:33:59 -0300

    #Update-vs1.13.0-ch1

---
+++ Test/Runtime/GlobalSuppressions.cs
@@ -0,0 +1,16 @@
+﻿//GlobalSuppressions.cs
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0028", Scope = "module")]
+[assembly: SuppressMessage("Style", "IDE0017", Scope = "module")]
+[assembly: SuppressMessage("Type Safety", "UNT0011", Scope = "module")]
+[assembly: SuppressMessage("Performance", "UNT0001", Scope = "module")]
+[assembly: SuppressMessage("Performance", "UNT0006", Scope = "module")]
+//[assembly: SuppressMessage("CodeQuality", "IDE0051:Remover membros privados não utilizados", Justification = "<Pendente>", Scope = "member", Target = "~M:Cobilas.Unity.Management.Resources.Asset.CobilasAssetManagement.Init")]
+//[assembly: SuppressMessage("CodeQuality", "IDE0051:Remover membros privados não utilizados", Justification = "<Pendente>", Scope = "member", Target = "~M:Cobilas.Unity.Management.Container.ContainerManager.Init")]
